35c9e94357d7cb7761547a10783fd8ba3a79b95c
maridabrins <maridabrins@hotmail.com>
2025-05-13 16:00:07 -0300

    docs: adicionando documentação swagger

--- src/main/java/com/crud/nails/controllers/ColecaoController.java
+++ src/main/java/com/crud/nails/controllers/ColecaoController.java
@@ -17,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.crud.nails.dto.ColecaoDTO;
 import com.crud.nails.services.ColecaoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping(value = "colecao")
 public class ColecaoController {
@@ -24,6 +28,11 @@ public class ColecaoController {
 	@Autowired
 	ColecaoService colecaoService;
 	
+	@Operation(description = "Busca o Esmalte pelo id,nome, cor e marca")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna o esmalte"),
+			@ApiResponse(responseCode = "400", description = "Não existe o esmalte com o id informado")
+	})
 	@GetMapping("/search")
     public ResponseEntity<List<ColecaoDTO>> search(
             @RequestParam("q") String termo) {
@@ -32,33 +41,62 @@ public class ColecaoController {
         return ResponseEntity.ok(resultados);
     }
 	
+	@Operation(description = "Mostra todos os esmaltes")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna os esmaltes na coleção"),
+			
+	})
+	
 	@GetMapping
 	public List<ColecaoDTO> buscarTodos(){
 		return colecaoService.buscarTodos();
 	}
 	
+	@Operation(description = "Busca o Esmalte pelo id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna o esmalte"),
+			@ApiResponse(responseCode = "400", description = "Não existe o esmalte com o id informado")
+	})
 	@GetMapping("/{id}")
 	public ResponseEntity<ColecaoDTO> buscarPorId(@PathVariable Long id){
 		ColecaoDTO dto = colecaoService.buscarPorId(id);
 		return ResponseEntity.ok(dto);
 	}
 	
+	
+	@Operation(description = "Cria um esmalte novo na coleção")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna o esmalte criado"),
+			@ApiResponse(responseCode = "400", description = "A requisição está incompleta ou malformada")
+	})
 	@PostMapping("/criar")
 	public ResponseEntity<ColecaoDTO> criarNovo (@RequestBody ColecaoDTO dto){
 		dto = colecaoService.criarNovo(dto);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@Operation(description = "Edita o Esmalte pelo id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna o esmalte editado"),
+			@ApiResponse(responseCode = "400", description = "Não existe o esmalte com o id informado")
+	})
 	@PutMapping("/editar/{id}")
 	public ResponseEntity<ColecaoDTO> editar (@PathVariable Long id, @RequestBody ColecaoDTO dto){
 		dto = colecaoService.editar(id,dto);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@Operation(description = "Deleta o Esmalte pelo id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Deleta o esmalte"),
+			@ApiResponse(responseCode = "400", description = "Não existe o esmalte com o id informado")
+	})
 	@DeleteMapping("/excluir/{id}")
 	public void delete(@PathVariable Long id) {
 		colecaoService.deletar(id);
 	}
+	
+	
 
 
 }
